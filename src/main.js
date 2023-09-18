148526a4b35dcf3c58af8dacb9f08e0c9f9bc956
Pablodr89 <padoro89@gmail.com>
2023-09-18 21:31:35 +0200

    main

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { plugin, defaultConfig } from '@formkit/vue'
 import config from '../formkit.config'
-
+// Firebase
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from './config/firebase'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(VueFire, {
+  firebaseApp,
+  modules: [VueFireAuth()],
+})
 app.use(plugin, defaultConfig(config))
 app.use(router)
 
